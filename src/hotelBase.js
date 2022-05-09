60b36febbab3774deecbc9be9567d2ec2ef32204
Isaac-Leonel <93339716+Isaac-Leonel@users.noreply.github.com>
2022-05-09 10:46:02 -0300

    Initial commit

---
+++ src/hotelBase.js
@@ -0,0 +1,40 @@
+const hotelBase = [
+  {
+    name: 'Lakewood',
+    quality: 3,
+    'Regular': {
+      weekday: 110,
+      weekend: 90
+    },
+    'Rewards': {
+      weekday: 80,
+      weekend: 80
+    }
+  },
+  {
+    name: 'Bridgewood',
+    quality: 4,
+    'Regular': {
+      weekday: 160,
+      weekend: 60
+    },
+    'Rewards': {
+      weekday: 110,
+      weekend: 50
+    }
+  },
+  {
+    name: 'Ridgewood',
+    quality: 5,
+    'Regular': {
+      weekday: 220,
+      weekend: 150
+    },
+    'Rewards': {
+      weekday: 100,
+      weekend: 40
+    }
+  }
+  ]
+  
+  module.exports = hotelBase;
---
+++ src/main.js
@@ -0,0 +1,80 @@
+const hotelBase = require('./hotelBase')
+
+// A function to separete the input, client from date list
+function separeteClientsAndDates (input) {
+    const [type, dates] = input.split(': ');
+    const listDates = dates.split(', ').map(date => new Date(date))//Split the dates and make a date object
+    return [type, listDates]
+}
+
+// To add the prices based on day | Array add with reduce
+function getAllPrices (type, hotel, listDates) {
+    return listDates.reduce((acc, date) => {
+        if (date.getDay() === 0 || date.getDay() === 6) {
+            return acc + hotel[type].weekend
+        } else {
+            return acc + hotel[type].weekday
+        }
+    }, 0)
+}
+
+// Get the cheapest price
+function getPriceArray (type, listDate){
+    var Highest = 1000
+    hotelBase.forEach((hotel, i, array) =>{
+        const priceHotel = getAllPrices(type, hotel, listDate)
+        switch(i){
+            case 0:
+                hotel1 = priceHotel
+                break;
+            case 1:
+                hotel2 = priceHotel
+                break;
+            case 2:
+                hotel3 = priceHotel
+                break;
+        }
+        if(priceHotel < Highest){
+            lowerPrice = priceHotel
+            Highest = Highest - 1000
+            Highest = priceHotel
+        }
+    })
+    return lowerPrice, hotel1, hotel2, hotel3
+}
+
+function getBestHotel(lowerPrice, hotel1, hotel2, hotel3){
+    // get a array only with names
+    getNames = []
+    hotelBase.forEach((hotel, i, array) =>{
+        getNames += array[i].name +  ","
+    })
+    // Split Array
+    const hotelNames = getNames.split(',')
+    // Checks equality of values ​​to return the hotel with higher quality
+    if(hotel1 === hotel2){ return hotelNames[1] }
+    if(hotel2 === hotel3){ return hotelNames[2] }
+    if(hotel3 === hotel1){ return hotelNames[2] }
+    else {
+        if(lowerPrice === hotel1){
+                return hotelNames[0]
+        }
+        if(lowerPrice === hotel2){
+                return hotelNames[1]
+        }
+        if(lowerPrice === hotel3){
+                return hotelNames[2]
+        }
+    }
+}
+
+
+
+function getCheapestHotel (input) { //DO NOT change the function's name.
+    const [type, listDates] = separeteClientsAndDates(input);
+    getPriceArray(type, listDates);
+    const bestHotel = getBestHotel(lowerPrice, hotel1, hotel2, hotel3)
+    return bestHotel;
+}
+
+exports.getCheapestHotel = getCheapestHotel
